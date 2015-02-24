24699e471ffa0424b06c7f2b8dc62b7df09aaeae
wilmoth77 <jeff@wilmoth77.com>
2015-02-24 10:30:12 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Moskow
-WordPress starter theme based on Underscores using Bootstrap with LESS for responsiveness, Grunt.js for automation and Bower for front end package management.
+bootstrap_s
+===========
+
+>WordPress starter theme based on Underscores using Bootstrap with LESS for responsiveness, Grunt.js for automation and Bower for front end package management.
+
+It is assumed that you already have Grunt, Bower and LESS installed on your local machine.
+
+You also need `grunt-init`, which you can install globally with 
+```
+npm install -g grunt-init
+```
+
+To clone the repository and add the template to grunt-init for automated project scaffolding run
+```
+git clone git@github.com:4Digits/bootstrap_s.git ~/.grunt-init/bootstrap_s
+```
+
+
+## Installation
+
+Install a copy of WordPress on your localhost. Cd into your themes folder with
+```
+cd /var/www/YourWordPressInstallation/wp-content/themes
+```
+and create a new, empty theme folder. 
+
+
+### 1. Generate the template files in your empty theme folder
+
+```
+grunt-init bootstrap_s
+```
+You should give your theme a title and function-prefix (e.g. MyTheme / mytheme).
+
+### 2. Install all required NPM modules
+
+```
+npm install
+```
+
+
+### 3. Install Bootstrap and other frontend dependencies with Bower
+
+To add your packages of choice, modify ```bower.json``` and run
+
+```
+bower install
+```
+
+
+### 4. Compile folder structure with Grunt
+
+```
+grunt compile
+```
+
+### 5. Monitor your filesystem for changes
+
+```
+grunt
+```
+In order to monitor file changes in real time without manually refreshing your browser, install [LiveReload](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?hl=en) for Chrome and run it in your tab.
+
+### Enjoy :)
---
+++ template.js
@@ -0,0 +1,72 @@
+/**
+ * bootstrap_s
+ * https://github.com/4digits/bootstrap_s
+ *
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+// Basic template description
+exports.description = 'Create a WordPress theme based on Bootstrap and Underscores.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = 'In order to rename all prefixes in the Underscores template, please give your theme a new name.';
+
+// Template-specific notes to be displayed after the question prompts.
+exports.after = 'Your template is complete! Please run `npm install`, `bower install` and then `grunt compile` to set up all dependencies and Bootstrap.';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template
+exports.template = function (grunt, init, done) {
+	init.process({}, [
+		// Prompt for these values.
+		init.prompt('title', 'bootstrap_s'),
+		{
+			name   : 'prefix',
+			message: 'PHP function prefix (alpha and underscore characters only)',
+			default: 'bootstrap_s'
+		},
+	], function( err, props ) {
+		props.keywords = [];
+		props.version = '1.0.0';
+		props.devDependencies = {
+			"grunt": "latest",
+    		"grunt-contrib-concat": "latest",
+    		"grunt-contrib-less": "latest",
+    		"grunt-contrib-uglify": "latest",
+    		"grunt-contrib-imagemin": "latest",
+    		"grunt-contrib-watch": "latest"
+		}; 
+
+		// Sanitize names where we need to for PHP/JS
+		props.name = props.title.replace(/\s+/g, '-').toLowerCase();
+		// Theme language
+		props.language = props.title.replace('/[^a-z_]/i', '').replace(/ /g, '').toLowerCase();
+		// Development prefix (i.e. to prefix PHP function names, variables)
+		props.prefix = props.prefix.replace('/[^a-z_]/i', '').toLowerCase();
+		// Development prefix in all caps (e.g. for constants)
+		props.prefix_caps = props.prefix.toUpperCase();
+		// An additional value, safe to use as a JavaScript identifier.
+		props.js_safe_name = props.name.replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
+		// An additional value that won't conflict with NodeUnit unit tests.
+		props.js_test_safe_name = props.js_safe_name === 'test' ? 'myTest' : props.js_safe_name;
+		props.js_safe_name_caps = props.js_safe_name.toUpperCase();
+
+		// Files to copy and process
+		var files = init.filesToCopy( props );
+
+		console.log( files );
+
+		// Actually copy and process files
+		init.copyAndProcess(files, props, {noProcess: 'screenshot.png'});
+
+		// Generate package.json file
+		init.writePackageJSON( 'package.json', props );
+
+		// Done!
+		done();
+	});
+};
